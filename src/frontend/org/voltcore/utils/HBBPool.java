2dd05319065a6bacd121dd081a2876cc2b09358f
wweiss <wweiss@voltdb.com>
2016-09-30 14:27:08 -0400

    ENG-10539: License for new SPI classes

--- src/frontend/org/voltcore/utils/HBBPool.java
+++ src/frontend/org/voltcore/utils/HBBPool.java
@@ -14,6 +14,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with VoltDB.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 package org.voltcore.utils;
 import java.lang.reflect.Method;
 import java.nio.ByteBuffer;
